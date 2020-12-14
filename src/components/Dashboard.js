54645fa9fa0dc71f1e0148f45c0b1e0b43738510
marilous07 <73708683+marilous07@users.noreply.github.com>
2020-12-14 21:51:49 +0200

    Update and rename Homepage.js to Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,18 +1,24 @@
 import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Stats from './Stats';
+import {Container} from "react-bootstrap";
+import DashboardCourses from './DashboardCourses';
 
-const Homepage = () => {
+
+const Dashboard = () => {
   return (
     <div>
+      <Container>
       <Jumbotron fluid className='mt-4 text-center'>
         <h1>Welcome to Code.Hub Dashboard!</h1>
         <h5>Manage everything and have fun!</h5>
       </Jumbotron>
 
       <Stats />
+    <DashboardCourses />
+    </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage;
+export default Dashboard;
