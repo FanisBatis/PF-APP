6b13261f1e4ea0b71584e597f70e92c719374a48
marilous07 <73708683+marilous07@users.noreply.github.com>
2020-12-17 21:23:39 +0200

    Update Stats.js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,42 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { API } from "../api";
-import { Row, Col,} from 'react-bootstrap';
-
+import { Row, Col, } from 'react-bootstrap';
+import '../Css Comp/Stats.css';
 
 const Stats = () => {
 
     const [stats, setStats] = useState([]);
 
-    const fetchData = () => {
-        fetch(API + "stats")
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error("Something went wrong ...");
-                }
-            })
-            .then((data) => {
-                setStats(data);
-                console.log(data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
     useEffect(() => {
+        const fetchData = () => {
+            fetch(API + "stats")
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                })
+                .then((data) => {
+                    setStats(data);
+                });
+        };
         fetchData();
     }, []);
     return (
         <div>
-            <Row>
-                {stats.map((item) => (
-                    <Col style={{ textAlign: "center" }}>
-                        <h5 style={{ textTransform: "uppercase" }}>{item.title}: <u style={{ color: "red" }}>{item.amount}</u></h5>
-                    </Col>
-                ))}
-            </Row>
-        </div>
+
+        <Row>
+        {stats.map((item) => (
+            <Col>
+        <div id="container">
+	<div id="cc">
+	<div class="circle" id="five"></div>
+	<div class="circle" id="four"></div>
+	<div class="circle" id="three"></div>
+	<div class="circle" id="two"></div>
+	<div class="circle" id="one">{item.title}: {item.amount}</div>
+    <div className="staats">
+                        
+                    </div>
+	</div>
+    
+</div>
+</Col> ))}
+</Row>
+</div>
     )
 }
 
